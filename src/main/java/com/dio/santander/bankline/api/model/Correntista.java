df671c78fdbd210faddb104fad80936054e47523
Helder Batista Lopes dos Santos <32435484+helderlopess@users.noreply.github.com>
2022-05-04 02:27:22 -0300

    mapeando as entidades com JPA
    realizado os mapeamentos utilizando os comando @ que importa o java persistence

--- src/main/java/com/dio/santander/bankline/api/model/Correntista.java
+++ src/main/java/com/dio/santander/bankline/api/model/Correntista.java
@@ -1,10 +1,29 @@
 package com.dio.santander.bankline.api.model;
 
+import javax.annotation.Generated;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tab_correntista")
 public class Correntista {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@Column(length = 20)	
 	private String cpf;
+	
+	@Column(length = 60)	
 	private String nome;
 	
+	
+	@Embedded //as informações que estiverem nessa classe "Conta" estarão na mesma tabela na tabela principal, a que possui a referencia a esta classe. é um relacionamento entre classes
 	private Conta conta;
 	public Conta getConta() {
 		return conta;
